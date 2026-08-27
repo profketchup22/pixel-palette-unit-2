c78792d35a75a83cd7214585d8936b5f4454d500
profketchup22 <profketchup22@gmail.com>
2026-08-26 20:40:32 -0500

    Initial Spring Boot project setup with MySQL connection

---
+++ java-spring-boot-back-end-app/pixel-palette-api/src/main/java/com/babs/pixel_palette_api/PixelPaletteApiApplication.java
@@ -0,0 +1,13 @@
+package com.babs.pixel_palette_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelPaletteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelPaletteApiApplication.class, args);
+	}
+
+}
---
+++ java-spring-boot-back-end-app/pixel-palette-api/src/test/java/com/babs/pixel_palette_api/PixelPaletteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babs.pixel_palette_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelPaletteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
